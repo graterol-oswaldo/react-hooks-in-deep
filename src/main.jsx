a927caa6adbe4282545b53f9392f60a0da25591d
Oswaldo Graterol <oswaldo.graterol@gmail.com>
2022-08-28 00:29:29 -0400

    feat: hooks in deep - customHooks: useFetch

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CustomHookForm } from './02-useEffect/CustomHookForm'
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { CustomHookForm } from './02-useEffect/CustomHookForm'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { HooksApp } from './HooksApp'
 // import {CounterApp} from './01-useState/CounterApp'
@@ -9,7 +10,7 @@ import { CustomHookForm } from './02-useEffect/CustomHookForm'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CustomHookForm />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <MultipleCustomHooks/>
+  // </React.StrictMode>
 )
