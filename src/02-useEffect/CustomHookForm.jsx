af1574e017b587bc4b5b0758eacaaadaaa7b6d93
Oswaldo Graterol <oswaldo.graterol@gmail.com>
2022-08-27 23:13:33 -0400

    feat:hooks in deep - customHook: improving useForm

--- src/02-useEffect/CustomHookForm.jsx
+++ src/02-useEffect/CustomHookForm.jsx
@@ -2,7 +2,7 @@ import { useForm } from "../hooks/useForm"
 
 export const CustomHookForm = () => {
 
-const { formState, onInputChange } = useForm( 
+const { formState, onInputChange, onResetForm } = useForm( 
   {
     username: '',
     email: '',
@@ -44,6 +44,7 @@ const {username,email,password} = formState;
         value={password} 
         onChange={ onInputChange }
         />
-    </>
+        <button className="btn btn-primary mt-2" onClick={ onResetForm }>Reset</button>
+      </>
   )
 }
